138b213290e5baaccdfa5f48483299f2ff62a257
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2018-10-07 23:18:55 +0530

    best algorithm

---
+++ Picking Numbers.py
@@ -0,0 +1,13 @@
+#https://www.hackerrank.com/challenges/picking-numbers/problem
+
+
+n=int(input())
+l=[int(i) for i in input().split()]
+maximum=0
+for i in l:
+    c=l.count(i)
+    d=l.count(i-1)
+    c=c+d
+    if c > maximum:
+        maximum=c
+print(maximum)
