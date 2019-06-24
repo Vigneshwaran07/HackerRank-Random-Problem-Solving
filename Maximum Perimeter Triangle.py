c6961d42463985ac93e92d0d27596c12693bd92f
Vigneshwaran V <43519088+Vigneshwaran07@users.noreply.github.com>
2019-06-24 17:26:14 +0530

    Create Maximum Perimeter Triangle.py

---
+++ Maximum Perimeter Triangle.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = sorted(list(map(int,input().split())))
+i = n-3
+while((i>=0) and (arr[i]+arr[i+1]<=arr[i+2])):
+    i -= 1
+if i>=0:
+    print(arr[i],arr[i+1],arr[i+2])
+else:
+    print(-1)
+    
